63834aca2eab82be38bde8966a457005d44de3cc
Nianjun Sun <sunnianjun@sphere-ex.com>
2022-01-07 13:22:04 +0800

    Refactor : try to change the command

--- mysql/master-with-1-slave/bin/startup.sh
+++ mysql/master-with-1-slave/bin/startup.sh
@@ -2,4 +2,4 @@
 
 #wget -N -P ../sql/ https://raw.githubusercontent.com/apache/shardingsphere/master/examples/src/resources/manual_schema.sql &&\
 cd ../conf &&
-docker compose up -d
+docker-compose up -d
