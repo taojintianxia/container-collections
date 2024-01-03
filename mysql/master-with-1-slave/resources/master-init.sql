568e66e3651bde35c3954b72b4537ec3a2569c69
孙念君 Nianjun Sun <sunnianjun@apache.org>
2024-01-03 11:00:39 +0800

    Refactor : update the master slave usage

--- mysql/master-with-1-slave/resources/master-init.sql
+++ mysql/master-with-1-slave/resources/master-init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'repl'@'%' IDENTIFIED WITH mysql_native_password BY 'root@12345';
+CREATE USER 'repl'@'%' IDENTIFIED WITH mysql_native_password BY 'Passw0rd';
 GRANT REPLICATION SLAVE ON *.* TO 'repl'@'%';
 flush privileges;
